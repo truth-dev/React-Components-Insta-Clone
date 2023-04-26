184b233f3c5197ae723ee48c8b306a38dcbfea00
truth-dev <Truth8510@hotmail.com>
2023-04-26 17:56:05 -0600

    finish instaProject

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
+import Posts from "./components/Posts/Posts";
+import SearchBar from "./components/SearchBar/SearchBar";
+import dummyData from "./dummy-data";
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
+  const [posts, setPosts] = useState(dummyData);
+  const[searchTerm, setSearchTerm] = useState(SearchBar);
+   
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -27,11 +32,30 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+     const updatedPosts = posts.map(post => {
+      if(post.id === postId) {
+        return {...post, likes: post.likes + 1}
+      }else{
+      return post;  
+      }
+     })
+        
+    /**
+     * map -> loop over an array, do something to ecah element, and return a new array 
+     * filter -> loop over and array, based on a condition, return particular elements into a new array
+     * reduce -> loop over an array and reduce it to a singualar value
+     * find -> loop over an array, based on a conditioon return the first element it finds that matches that condition 
+     */
+        
+   setPosts(updatedPosts);
+
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar/>
+      <Posts posts={posts} likePost={likePost}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
